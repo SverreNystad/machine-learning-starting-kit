33d1454c6547767463df24ce90755771e5d3ee94
Sverre Nystad <sverrenystad@gmail.com>
2024-03-24 18:43:49 +0100

    feat: add feature engineering functions to prepare data for the model

---
+++ src/features/feature_engineering.py
@@ -0,0 +1,19 @@
+""" This module contains functions to prepare the data for the model. """
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+def engineer_features(features: pd.DataFrame) -> pd.DataFrame:
+    """
+    Conduct the required feature engineering.
+
+    Args:
+        features: The features to engineer.
+
+    Returns:
+        The engineered features.
+    """
+    # TODO Add relevant feature engineering
+    return features
+
