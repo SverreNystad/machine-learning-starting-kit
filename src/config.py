5c0c288e5c596a79d34b36748a2f15cd09310e85
Sverre Nystad <sverrenystad@gmail.com>
2024-03-24 18:42:32 +0100

    feat: add config file with file paths and target features

---
+++ src/config.py
@@ -0,0 +1,12 @@
+""" Configuration file for the task5 application. """
+
+# Training data file path
+TRAINING_DATA_FILE = "../data/Transaction data training.csv"
+TEST_DATA_FILE = "../data/Transaction data training.csv"
+
+# Output file paths
+PREDICTIONS_PATH = "../results/predictions/"
+FIGURE_PATH = "../results/figures/"
+
+# Target features
+TARGET_FEATURES = []
