8b6c1c420b88b34f4a45038de60ddb7ea52ef2f0
Sverre Nystad <sverrenystad@gmail.com>
2024-03-25 12:37:00 +0100

    feat: Add REPORT_PATH to config.py

--- src/config.py
+++ src/config.py
@@ -9,6 +9,7 @@ TEST_DATA_FILE = "../data/Transaction data training.csv"
 # Output file paths
 PREDICTIONS_PATH = "../results/predictions/"
 FIGURE_PATH = "../results/figures/"
+REPORT_PATH = "../results/reports/"
 
 # Target features
 TARGET_FEATURES = []
