57efa3f3bd1af440cfe01b0b4be3b43d14e3cab2
Sverre Nystad <sverrenystad@gmail.com>
2024-03-26 14:32:56 +0100

    test: Add tests for prepare_data with edge cases of val and test size

--- test/test_ml_service.py
+++ test/test_ml_service.py
@@ -56,3 +56,46 @@ def test_prepare_data_holdout_size(loader: DataLoader):
 
     with pytest.raises(ValueError):
         prepare_data(validation_size=0.2, test_size=0.8, loader=loader)
+
+
+def test_prepare_data_with_no_holdout_data(loader: DataLoader):
+    x_train, x_val, x_test, y_train, y_val, y_test = prepare_data(
+        validation_size=0.0, test_size=0.0, loader=loader
+    )
+    total_samples = loader.load_raw_data()[0].shape[0]
+    assert len(x_train) == total_samples
+    assert len(x_val) == 0
+    assert len(x_test) == 0
+    assert len(y_train) == total_samples
+    assert len(y_val) == 0
+    assert len(y_test) == 0
+
+
+def test_prepare_data_with_no_test_data(loader: DataLoader):
+    x_train, x_val, x_test, y_train, y_val, y_test = prepare_data(
+        validation_size=0.2, test_size=0.0, loader=loader
+    )
+    total_samples = loader.load_raw_data()[0].shape[0]
+    expected_train_size = int(total_samples * 0.8)
+    expected_validation_size = int(total_samples * 0.2)
+    assert len(x_train) == expected_train_size
+    assert len(x_val) == expected_validation_size
+    assert len(x_test) == 0
+    assert len(y_train) == expected_train_size
+    assert len(y_val) == expected_validation_size
+    assert len(y_test) == 0
+
+
+def test_prepare_data_with_no_validation_data(loader: DataLoader):
+    x_train, x_val, x_test, y_train, y_val, y_test = prepare_data(
+        validation_size=0.0, test_size=0.2, loader=loader
+    )
+    total_samples = loader.load_raw_data()[0].shape[0]
+    expected_train_size = int(total_samples * 0.8)
+    expected_test_size = int(total_samples * 0.2)
+    assert len(x_train) == expected_train_size
+    assert len(x_val) == 0
+    assert len(x_test) == expected_test_size
+    assert len(y_train) == expected_train_size
+    assert len(y_val) == 0
+    assert len(y_test) == expected_test_size
