103f424e0108b93bd0521766d1b25f6daba62d3e
Sverre Nystad <sverrenystad@gmail.com>
2024-03-28 11:41:28 +0100

    docs: Remove unnecessary comments in data_loader.py

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -52,8 +52,6 @@ class MockDataLoader(DataLoader):
     ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """
         Generating training and test data for
-        training and testing the neural network.
-        Do not modify this function.
         """
         X_train = np.random.rand(n_train, 2) * 2 - 1
         y_train = self._noisy_func(X_train)
@@ -65,14 +63,12 @@ class MockDataLoader(DataLoader):
     def _noisy_func(self, X: np.ndarray, epsilon: float = 0.075) -> np.ndarray:
         """
         Add Gaussian noise to the data generating function.
-        Do not modify this function.
         """
         return self._func(X) + np.random.randn(len(X)) * epsilon
 
     def _func(self, X: np.ndarray) -> np.ndarray:
         """
         The data generating function.
-        Do not modify this function.
         """
         return 0.3 * X[:, 0] + 0.6 * X[:, 1] ** 2
 
