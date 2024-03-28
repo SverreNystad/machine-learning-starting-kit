1cf1d7de0254f8bf1f24f545b1e078de02820ac7
Sverre Nystad <sverrenystad@gmail.com>
2024-03-28 11:41:13 +0100

    refactor: pull save predictions out of post processing.py and into ml_service

--- src/features/post_processor.py
+++ src/features/post_processor.py
@@ -1,34 +1,6 @@
 """ This module contains functions to post-process the data."""
 
-from datetime import datetime
 import pandas as pd
-from src.config import PREDICTIONS_PATH
-
-
-def save_predictions(
-    predictions: pd.DataFrame, file_name: str, shallHaveIndexColum: bool = True
-) -> None:
-    """
-    Save the predictions to a file and post process predictions.
-
-    Args:
-        predictions: The predictions to save.
-        file_name: The name of the file to save the predictions to.
-    """
-
-    # Post-process the predictions
-    predictions = post_process(predictions)
-
-    # Save the predictions to a CSV file
-    predictions_filename = PREDICTIONS_PATH + file_name
-    if not predictions_filename.endswith(".csv"):
-        predictions_filename += ".csv"
-
-    # Add time to the filename
-    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-    predictions_filename = predictions_filename.replace(".csv", f"_{current_time}.csv")
-
-    predictions.to_csv(predictions_filename, index=shallHaveIndexColum)
 
 
 def post_process(predictions: pd.DataFrame) -> pd.DataFrame:
--- src/ml_service.py
+++ src/ml_service.py
@@ -2,8 +2,11 @@
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from datetime import datetime
+from src.config import PREDICTIONS_PATH
 from src.data.data_loader import DataLoader, create_data_loader
 from src.features.feature_engineering import engineer_features
+from src.features.post_processor import post_process
 
 
 def prepare_data(
@@ -108,3 +111,29 @@ def prepare_test_data() -> pd.DataFrame:
     engineered_features = engineer_features(test_data)
 
     return engineered_features
+
+
+def save_predictions(
+    predictions: pd.DataFrame, file_name: str, shallHaveIndexColum: bool = True
+) -> None:
+    """
+    Save the predictions to a file and post process predictions.
+
+    Args:
+        predictions: The predictions to save.
+        file_name: The name of the file to save the predictions to.
+    """
+
+    # Post-process the predictions
+    predictions = post_process(predictions)
+
+    # Save the predictions to a CSV file
+    predictions_filename = PREDICTIONS_PATH + file_name
+    if not predictions_filename.endswith(".csv"):
+        predictions_filename += ".csv"
+
+    # Add time to the filename
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    predictions_filename = predictions_filename.replace(".csv", f"_{current_time}.csv")
+
+    predictions.to_csv(predictions_filename, index=shallHaveIndexColum)
